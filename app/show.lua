df664c198cbf21e825e1cd0494c6b829b73abda1
lanbiaowan <752502067@qq.com>
2019-06-12 11:28:51 +0800

    test

--- app/show.lua
+++ app/show.lua
@@ -5,7 +5,11 @@ local template = require "resty.template"
 
 --template.render(ngx.var.command ..".html",{ message = ngx.var.params})
 --local tpl = ngx.var.command .. ".html"
+
 --test1
+
+--test
+
 
 template.render(ngx.var.command ..".html",{param = ngx.var.arg_param,param2 = ngx.var.arg_param2})
 --ngx.say(ngx.var.command .. ".html")
